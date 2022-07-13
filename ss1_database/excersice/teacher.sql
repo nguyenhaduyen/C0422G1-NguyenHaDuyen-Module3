3f2c4d42d6dbaed92668e40db31322ccd7ebf8ae
nguyenhaduyen <nguyenhaduyen23@gmail.com>
2022-07-13 14:11:39 +0700

    bt

--- ss1_database/excersice/teacher.sql
+++ ss1_database/excersice/teacher.sql
@@ -1,3 +1,5 @@
+use student_management;
+
 create table teacher (
 id_teacher int auto_increment,
 name varchar(50),
@@ -5,5 +7,18 @@ age int,
 nation varchar(55),
 primary key (id_teacher)
 );
-insert into teacher(name,age,nation) value ("Nggu",15,"VN");
-select * from teacher;
+insert into teacher(name,age,nation) 
+value ("Nggu",15,"VN");
+select *
+ from teacher;
+
+use student_management;
+create table class (
+id_class int auto_increment,
+name_class varchar (55),
+primary key (id_class)
+);
+insert into
+ class (name_class) value ("C04");
+select *
+ from class;
