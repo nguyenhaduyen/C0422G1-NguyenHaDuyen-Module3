ffe500d4229d529db54da6b7736f13b730930850
nguyenhaduyen <nguyenhaduyen23@gmail.com>
2022-07-15 17:11:38 +0700

    bt

--- ss2_thiet_ke_va_tao_CSDL/excersice/bai4.sql
+++ ss2_thiet_ke_va_tao_CSDL/excersice/bai4.sql
@@ -5,32 +5,74 @@ CREATE DATABASE quan_ly_ban_hang;
 USE quan_ly_ban_hang;
 
 CREATE TABLE customer (
-    customer_id INT PRIMARY KEY,
+    customer_id INT auto_increment KEY,
     customer_name VARCHAR(30) NOT NULL,
     customer_age INT
 );
+INSERT INTO customer (customer_name,customer_age) VALUES ("Minh Quan",10) , ("Ngoc Oanh",20), ("Hong Ha",50);
 
 CREATE TABLE `order` (
-    order_id INT PRIMARY KEY,
+    order_id INT PRIMARY KEY auto_increment,
     customer_id INT NOT NULL,
-    order_date DATETIME,
+    order_date DATE,
     order_total_price FLOAT,
     FOREIGN KEY (customer_id)
         REFERENCES customer (customer_id)
 );
+INSERT INTO `order` (customer_id,order_date,order_total_price) VALUES (1,"2006-3-21",NULL),(2,"2006-3-23",NULL),(1,"2006-3-16",NULL);
 
 CREATE TABLE product (
-    product_id INT PRIMARY KEY,
+    product_id INT PRIMARY KEY auto_increment,
     product_name VARCHAR(50),
     product_price FLOAT
 );
+
+INSERT INTO product(product_name,product_price) VALUES ("May Giat",3),("Tu Lanh",5),("Dieu Hoa",7),("Quat",1),("Bep Dien",2);
 
 CREATE TABLE order_detail (
     order_id INT,
     product_id INT,
     order_quanity INT,
+    primary key (order_id,product_id),
     FOREIGN KEY (order_id)
         REFERENCES `order` (order_id),
     FOREIGN KEY (product_id)
         REFERENCES product (product_id)
 );
+
+INSERT INTO order_detail(order_id,product_id,order_quanity) VALUE (1,1,3),(1,3,7),(1,4,2),(2,1,1),(3,1,8),(2,5,4),(2,3,3);
+
+-- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
+SELECT o.customer_id, o.order_date,o.order_total_price
+FROM `order` o;
+
+-- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
+select c.customer_name, p.product_name
+from customer c
+join `order` o
+on c.customer_id = o.customer_id
+join order_detail od
+on o.order_id = od.order_id
+join product p
+on od.product_id = p.product_id;
+    
+--  Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+SELECT 
+    c.customer_name
+FROM
+    customer c
+        LEFT JOIN
+    `order` o ON c.customer_id = o.customer_id
+WHERE
+    o.order_date IS NULL; 
+
+-- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn.
+--  Giá bán của từng loại được tính = odQTY*pPrice)
+;
+select o.order_id, o.order_date, sum((p.product_price*od.order_quanity)) as price
+from `order` o join order_detail od 
+on o.order_id = od.order_id
+join product p
+on od.product_id = p.product_id
+group by o.order_id;
+
---
+++ ss3/practice/bai1.sql
@@ -0,0 +1,104 @@
+DROP DATABASE IF EXISTS quan_li_sinh_vien1;
+
+CREATE DATABASE quan_li_sinh_vien1;
+
+USE quan_li_sinh_vien1;
+
+CREATE TABLE class (
+    class_id INT AUTO_INCREMENT PRIMARY KEY,
+    class_name VARCHAR(20),
+    start_date DATE,
+    `status` INT
+);
+
+INSERT INTO class (class_name, start_date,`status`) 
+VALUES ("A1", "2008-12-20",1),("A2","2008-12-22",1),("B3", CURRENT_DATE,0);
+
+SELECT *
+FROM class WHERE class_name = "A1";
+
+CREATE TABLE student (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(20),
+    address VARCHAR(20),
+    phone VARCHAR(20),
+    `status` INT,
+    class_id INT,
+    FOREIGN KEY (class_id)
+       REFERENCES class (class_id)
+);
+
+INSERT INTO student (student_name,address,phone,`status`,class_id) 
+VALUES ("Hung","Ha Noi","0912113113",1,1),("Hoa","Hai Phong",NULL,1,1),("Manh","HCM", "	
+0123123123",0,2);
+
+SELECT *
+FROM student;
+
+SELECT *
+FROM student WHERE `status` = TRUE;
+
+SELECT S.student_id, S.student_name, C.class_name as a
+FROM student S  JOIN  class c on S.class_id = C.class_id
+WHERE C.class_name = "A1";
+
+SELECT S.student_id, S.student_name, Sub.sub_name, M.mark
+FROM Student S join Mark M on S.student_id = M.student_id join Subject Sub on M.sub_id = Sub.sub_id
+WHERE Sub.sub_name = "CF";
+
+CREATE TABLE subject (
+    sub_id INT AUTO_INCREMENT PRIMARY KEY,
+    sub_name VARCHAR(20),
+    credit INT,
+    `status` INT
+);
+
+INSERT INTO subject (sub_name,credit,`status`) 
+VALUES ("CF",5,1),("C",6,1),("HDJ",5,1),("RDBMS",10,1);
+
+SELECT *
+FROM subject WHERE credit <10;
+
+CREATE TABLE mark (
+    mark_id INT AUTO_INCREMENT PRIMARY KEY,
+    sub_id INT,
+    student_id INT,
+    mark INT,
+    exam_times INT,
+    FOREIGN KEY (sub_id)
+        REFERENCES subject (sub_id),
+    FOREIGN KEY (student_id)
+        REFERENCES student (student_id)
+); 
+
+INSERT INTO  mark (sub_id,student_id,mark,exam_times) VALUES (1,1,8,1),(1,2,10,2),(2,1,12,1);
+
+-- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
+SELECT *
+FROM student s
+WHERE s.student_name like "H%";
+
+-- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
+SELECT *
+FROM class c
+WHERE month(c.start_date)=12;
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
+SELECT *
+FROM subject s
+WHERE s.credit BETWEEN 3 AND 5; 
+-- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+SET sql_safe_updates = 0;
+UPDATE student SET class_id = 2 WHERE student_name = "Hung";
+SET sql_safe_updates = 1;
+SELECT *
+FROM student;
+-- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần, nếu trùng sắp theo tên tăng dần.
+SELECT 
+    s.student_name, sub.sub_name, m.mark
+FROM
+    student s
+        JOIN
+    mark m ON s.student_id = m.student_id
+        JOIN
+    subject sub ON m.sub_id = sub.sub_id
+ORDER BY m.mark , m.mark DESC;
